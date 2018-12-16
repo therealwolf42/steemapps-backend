19a1430c86cb8bc43e6c2c85bd1935ccb830021f
therealwolf <therealwolfynator@gmail.com>
2018-12-16 01:51:23 +0100

    log

--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,7 @@ export let start = async () => {
 let main = async () => {
   while (true) {
     try {
+      console.log(process.env.NODE_ENV)
       if (process.env.NODE_ENV === 'production') {
         // Update Global Properties for calculation of Vests to Steempower
         console.log('update_global_properties')
