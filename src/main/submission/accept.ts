b9637e7e1b5da28cd6838289ac92a0d0cf3be84e
therealwolf <therealwolfynator@gmail.com>
2018-12-29 17:21:31 +0100

    new submission bug fix

--- src/main/submission/accept.ts
+++ src/main/submission/accept.ts
@@ -18,13 +18,13 @@ export const convert_accepted_submissions = async () => {
         }
 
         let app = (await db_app.find(submission.name))[0]
+        submission.approved = approved = true
+        let submission_object = create_object(submission)
         if (app) {
-          await change_existing_app(app, submission)
+          await change_existing_app(app, submission_object)
         } else {
-          submission.approved = true
-          await ModelApp.App.create(submission)
+          await ModelApp.App.create(submission_object)
         }
-        approved = true
         await submission.remove()
       }
     }
@@ -33,17 +33,25 @@ export const convert_accepted_submissions = async () => {
   }
 }
 
-const ignore_keys = ['_id', 'approved', '__v']
+const ignore_keys = ['_id', '__v']
 
 export const change_existing_app = async (app, submission) => {
-  for (let key of Object.keys(submission.toJSON())) {
-    if (ignore_keys.includes(key)) {
-      continue
-    } else if (key === 'createdAt') {
+  for (let key of Object.keys(submission)) {
+    if (key === 'createdAt') {
       app['last_update'] = submission[key]
     } else {
       app[key] = submission[key]
     }
   }
   await app.save()
+}
+
+export const create_object = (submission) => {
+  let x = {}
+  for (let key of Object.keys(submission.toJSON())) {
+    if (!ignore_keys.includes(key)) {
+      x[key] = submission[key]
+    }
+  }
+  return x
 }
